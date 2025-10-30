b95fb65667f4f41a60f3313f3dfc9d1e7a63e429
mj_github <maurice.johnson9911@gmail.com>
2025-10-29 21:55:08 -0400

    refactoring

--- src/main/java/com/main/npb/controller/UIControllers.java
+++ src/main/java/com/main/npb/controller/UIControllers.java
@@ -1,11 +1,12 @@
 package com.main.npb.controller;
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UIControllers {
