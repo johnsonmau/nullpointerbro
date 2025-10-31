2305f6e246381a42e8feb7d4100f5f07b72c501b
mj_github <maurice.johnson9911@gmail.com>
2025-10-30 21:56:46 -0400

    refactor

--- src/main/java/com/main/npb/controller/UIControllers.java
+++ src/main/java/com/main/npb/controller/UIControllers.java
@@ -19,8 +19,10 @@ public class UIControllers {
         String userAgent = request.getHeader("User-Agent");
         String referer = request.getHeader("Referer");
 
-        // Log or save to database
-        log.info("Visitor: IP={}, UA={}, Referer={}", ip, userAgent, referer);
+        if (!userAgent.contains("Uptime-Kuma")){
+            // Log or save to database
+            log.info("Visitor: IP={}, User Agent={}, Referer={}", ip, userAgent, referer);
+        }
 
         return "index";
     }
